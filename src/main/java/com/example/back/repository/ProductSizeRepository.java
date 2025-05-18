7562ec8cba2c2324f1957e0e653a652f3847436f
duynguyen14 <duy68327@gmail.com>
2025-05-18 16:50:10 +0700

    tx1

--- src/main/java/com/example/back/repository/ProductSizeRepository.java
+++ src/main/java/com/example/back/repository/ProductSizeRepository.java
@@ -2,6 +2,7 @@ package com.example.back.repository;
 
 import com.example.back.entity.Product;
 import com.example.back.entity.ProductSize;
+import com.example.back.entity.ShoppingCartDetail;
 import com.example.back.entity.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,5 @@ import java.util.Optional;
 
 public interface ProductSizeRepository extends JpaRepository<ProductSize,Integer> {
     Optional<ProductSize> findBySizeAndProduct(Size size, Product product);
+//    Optional<ProductSize> findByShoppingCartDetail(ShoppingCartDetail shoppingCartDetail);
 }
--- src/main/java/com/example/back/repository/ShoppingCartDetailRepository.java
+++ src/main/java/com/example/back/repository/ShoppingCartDetailRepository.java
@@ -6,9 +6,12 @@ import com.example.back.entity.ShoppingCartDetail;
 import com.example.back.entity.ShoppingCartDetailId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ShoppingCartDetailRepository extends JpaRepository<ShoppingCartDetail, ShoppingCartDetailId> {
 
     Optional<ShoppingCartDetail> findByProductSizeAndShoppingCart(ProductSize productSize, ShoppingCart shoppingCart);
+
+    List<ShoppingCartDetail> findByShoppingCart(ShoppingCart shoppingCart);
 }
